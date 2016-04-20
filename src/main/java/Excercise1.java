6aefdfa2da1691bc4819e5d52fb3965ee6b6a23a
Ramesh Sukka <ramesh.sukka@gmail.com>
2016-04-20 10:45:18 +0100

    initial commit

---
+++ src/main/java/Excercise1.java
@@ -0,0 +1,10 @@
+package main.java;
+
+
+public class Excercise1 {
+
+    public static void main(String args[]) {
+
+        System.out.println("Welcome to Hack Night");
+    }
+}
---
+++ src/main/scala/Excercise1.scala
@@ -0,0 +1,13 @@
+package main.scala
+
+
+object Excercise1 {
+
+  //TODO Write standalone application to print something to output screen
+  /**
+    *   We can do it in two ways
+    *    1) Extend the object with App class and write statement
+    *    2) Write main class similar to Java refer  Excercise1.java for more help
+    **/
+
+}
